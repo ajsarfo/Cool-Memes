ee92e1e6e052b3b887ad020f4ec04fb2af6d4bb3
Sarfo <kakrajoe123@gmail.com>
2021-12-14 18:30:36 +0000

    **Changed night mode colors

--- app/src/main/java/com/sarftec/coolmemes/MemeApp.kt
+++ app/src/main/java/com/sarftec/coolmemes/MemeApp.kt
@@ -1,8 +1,11 @@
 package com.sarftec.coolmemes
 
 import androidx.multidex.MultiDexApplication
+import com.google.firebase.FirebaseApp
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class MemeApp : MultiDexApplication() {
+
+
 }
